1a9109c7c6b92192482a478aa0deda9769e9f2c1
SamiRaito <mohamed7gp@gmail.com>
2020-05-01 12:10:56 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 45,
+      stretch: 0,
+      depth: 150,
+      modifier: 1,
+      slideShadows : true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
